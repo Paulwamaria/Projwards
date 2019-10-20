4e143406373ffb991abbebe189d5527aba4c9309
Paulwamaria <paulwamaria@gmail.com>
2019-10-20 14:16:21 +0300

    Registers the Project, Review and Vote class

--- wards/admin.py
+++ wards/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Project, Review, Vote
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Review)
+admin.site.register(Vote)
