4468226d2cbbdf732c27e4fb7bf53fb25b16b0e3
Paulwamaria <paulwamaria@gmail.com>
2019-10-20 15:49:35 +0300

    define a view for registration

--- users/views.py
+++ users/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
 from django.contrib.auth.models import User
+from .forms import ProjwardsRegistrationForm
+from django.contrib import messages
+from .models import Profile
 
-# Create your views here.
-
-
+def register(request):
+    if request.method == 'POST':
+        form = ProjwardsRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data['email']
+            fullName = form.cleaned_data['fullName']
+            username = form.cleaned_data.get('username')
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
+            messages.success(request, f'Account created for {username}')
+            return redirect('home')
+    else:
+        form = ProjwardsRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
