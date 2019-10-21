0dfaafea76daafc1ebbb6ea30a15cbfdf4efda6f
Paulwamaria <paulwamaria@gmail.com>
2019-10-21 18:05:40 +0300

    Add first name and last name fields

--- users/forms.py
+++ users/forms.py
@@ -8,7 +8,8 @@ from .models import Profile
 class ProjwardsRegistrationForm(UserCreationForm):
     username = forms.CharField(max_length=60,required = True, label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Username'}))
     email = forms.EmailField(required = True,label='',widget=forms.TextInput(attrs = {'class':'form-control','placeholder':'Email'}))
-    name = forms.CharField(max_length=256,required = True, label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Your Full Name'}))
+    first_name = forms.CharField(max_length=256,required = True, label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}))
+    last_name = forms.CharField(max_length=256,required = True, label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}))
     
     password1 = forms.CharField(max_length = 30,required = True, label='', widget = forms.TextInput(attrs={
         "class":"form-control",
@@ -25,12 +26,13 @@ class ProjwardsRegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['email','name','username','password1','password2']
+        fields = ['email','first_name', 'last_name','username','password1','password2']
 
     def save(self, commit=True):
         user =super(ProjwardsRegistrationForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
-        user.name = self.cleaned_data['name']
+        user.last_name = self.cleaned_data['first_name']
+        user.first_name = self.cleaned_data['last_name']
         user.username = self.cleaned_data['username']
         user.password1 = self.cleaned_data['password1']
         user.password2 = self.cleaned_data['password2']
@@ -44,12 +46,13 @@ class ProjwardsRegistrationForm(UserCreationForm):
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField(required = True,label='',widget=forms.TextInput(attrs = {'class':'form-control','placeholder':'Email'}))
-    name = forms.CharField(max_length=256,required = True, label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Your Full Name'}))
+    first_name = forms.CharField(max_length=256,required = True, label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}))
+    last_name = forms.CharField(max_length=256,required = True, label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}))
     username = forms.CharField(max_length=60,required = True, label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Username'}))
 
     class Meta:
         model = User
-        fields = ['email','name','username']
+        fields = ['email','first_name', 'last_name','username']
 
 
 class ProfileUpdateForm(forms.ModelForm):
