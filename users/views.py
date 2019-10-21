c9b5dbffaabb93c7cbb178d77be12f68da89330b
Paulwamaria <paulwamaria@gmail.com>
2019-10-21 14:55:56 +0300

    Add funtion to update profile

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
-
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .forms import ProjwardsRegistrationForm
+from .forms import ProjwardsRegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from .models import Profile
 
@@ -11,7 +11,7 @@ def register(request):
         if form.is_valid():
             form.save()
             email = form.cleaned_data['email']
-            fullName = form.cleaned_data['fullName']
+            name = form.cleaned_data['name']
             username = form.cleaned_data.get('username')
             password1 = form.cleaned_data['password1']
             password2 = form.cleaned_data['password2']
@@ -20,3 +20,27 @@ def register(request):
     else:
         form = ProjwardsRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance = request.user)
+        
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance = request.user.profile)    
+        if u_form.is_valid and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+    else:
+        u_form = UserUpdateForm(instance = request.user)
+        
+        p_form = ProfileUpdateForm(instance = request.user.profile)
+
+
+    context={
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request, 'users/profile.html',context)
+
